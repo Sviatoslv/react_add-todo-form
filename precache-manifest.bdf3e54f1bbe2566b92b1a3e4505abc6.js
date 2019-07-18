8b0ed318b53cf3a40a0c3b91832274f469017f2a
Пользователь <Sviatoslv>
2019-07-18 19:45:53 +0300

    Updates

---
+++ precache-manifest.bdf3e54f1bbe2566b92b1a3e4505abc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058384c4c5c26dccbd74037a90d89fb1",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "4ba4557dd832cc96119d",
+    "url": "/react_add-todo-form/static/css/main.771c89e6.chunk.css"
+  },
+  {
+    "revision": "dc6770f0d067976e4645",
+    "url": "/react_add-todo-form/static/js/2.75b69a17.chunk.js"
+  },
+  {
+    "revision": "4ba4557dd832cc96119d",
+    "url": "/react_add-todo-form/static/js/main.50a69be7.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
